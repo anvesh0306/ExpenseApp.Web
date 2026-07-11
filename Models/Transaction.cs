989bfd765d96c99c98977c5594cb09a57a8591cc
anvesh0306 <anveshaus2020@gmail.com>
2026-07-11 14:01:33 +1000

    updated Transactions.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -4,7 +4,7 @@ public class Transaction
 {
     public int Id { get; set; }
 
-    public int Amount { get; set; }
+    public decimal Amount { get; set; }
 
     public string Description { get; set; } = string.Empty;
 
